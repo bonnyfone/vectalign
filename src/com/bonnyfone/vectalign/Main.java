a14faec75331bb056c884dbadfa8bfab3942ee0f
bonnyfone@gmail.com <bonnyfone@gmail.com>
2015-08-21 15:24:52 +0200

    added 'read from file' commandline option

--- src/com/bonnyfone/vectalign/Main.java
+++ src/com/bonnyfone/vectalign/Main.java
@@ -3,7 +3,10 @@ package com.bonnyfone.vectalign;
 
 import org.apache.commons.cli.*;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * VectorCompatAlign commandline tool
@@ -30,11 +33,13 @@ public class Main {
 
     public static void main(String args[]) throws IOException {
         //TEST input args
-        String star = "M 48,54 L 31,42 15,54 21,35 6,23 25,23 25,23 25,23 25,23 32,4 40,23 58,23 42,35 z";
-        String arrow = "M 12, 4 L 10.59,5.41 L 16.17,11 L 18.99,11 L 12,4 z M 4, 11 L 4, 13 L 18.99, 13 L 20, 12 L 18.99, 11 L 4, 11 z M 12,20 L 10.59, 18.59 L 16.17, 13 L 18.99, 13 L 12, 20z";
+        /*
+            String star = "M 48,54 L 31,42 15,54 21,35 6,23 25,23 25,23 25,23 25,23 32,4 40,23 58,23 42,35 z";
+            String arrow = "M 12, 4 L 10.59,5.41 L 16.17,11 L 18.99,11 L 12,4 z M 4, 11 L 4, 13 L 18.99, 13 L 20, 12 L 18.99, 11 L 4, 11 z M 12,20 L 10.59, 18.59 L 16.17, 13 L 18.99, 13 L 12, 20z";
 
-        args = new String[]{"-s", star, "--end", arrow};
-        //args = new String[]{"-h"};
+            //args = new String[]{"-s", star, "--end", arrow};
+            //args = new String[]{"-h"};
+        */
 
         String fromSequence = null;
         String toSequence = null;
@@ -56,12 +61,21 @@ public class Main {
                 return;
             }
 
+            File tmpFile;
             if(commandLine.hasOption(OPTION_FROM)){
                 fromSequence = commandLine.getOptionValue(OPTION_FROM);
+
+                tmpFile = new File(fromSequence);
+                if(tmpFile.isFile() && tmpFile.exists())
+                    fromSequence = readSequenceFromFile(tmpFile);
             }
 
             if(commandLine.hasOption(OPTION_TO)){
                 toSequence = commandLine.getOptionValue(OPTION_TO);
+
+                tmpFile = new File(toSequence);
+                if(tmpFile.isFile() && tmpFile.exists())
+                    toSequence = readSequenceFromFile(tmpFile);
             }
 
 
@@ -73,11 +87,25 @@ public class Main {
             System.out.println("\n# new START sequence:  \n" + ANSI_BLUE + align[0] + ANSI_RESET);
             System.out.println("\n# new END sequence:  \n" + ANSI_BLUE +align[1] + ANSI_RESET);
 
+            System.out.println("\nNow, substitute your original sequences with the new aligned ones in your Android project.\n");
+
 
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
+    }
+
+    private static String readSequenceFromFile(File f){
+        byte[] encoded = new byte[0];
+        try {
+            encoded = Files.readAllBytes(Paths.get(f.toURI()));
+            return new String(encoded, "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     /**
@@ -90,13 +118,13 @@ public class Main {
         options.addOption(OptionBuilder.withLongOpt("start")
                 .withDescription("The VectorDrawable sequence from which we start the morphing")
                 .hasArg()
-                .withArgName("SEQUENCE")
+                .withArgName("SEQUENCE | FILE")
                 .create(OPTION_FROM));
 
         options.addOption(OptionBuilder.withLongOpt("end")
                 .withDescription("The VectorDrawable sequence which ends the morphing")
                 .hasArg()
-                .withArgName("SEQUENCE")
+                .withArgName("SEQUENCE | FILE")
                 .create(OPTION_TO));
 
         options.addOption(OptionBuilder.withLongOpt("version")
